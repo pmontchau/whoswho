039f540ba21635cf3fbf77e48c7f79c639121283
mopauline <genesis@LAP218300055.local>
2014-07-26 18:03:43 +0200

    Xcode project created

---
+++ whoswho/whoswho/WWViewController.h
@@ -0,0 +1,13 @@
+//
+//  WWViewController.h
+//  whoswho
+//
+//  Created by Pauline on 26/07/2014.
+//  Copyright (c) 2014 Pauline. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface WWViewController : UIViewController
+
+@end
---
+++ whoswho/whoswhoTests/whoswhoTests.m
@@ -0,0 +1,34 @@
+//
+//  whoswhoTests.m
+//  whoswhoTests
+//
+//  Created by Pauline on 26/07/2014.
+//  Copyright (c) 2014 Pauline. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface whoswhoTests : XCTestCase
+
+@end
+
+@implementation whoswhoTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
